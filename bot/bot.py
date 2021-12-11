a9cb28682c384198f6f0b28190244e41b0f99b82
Zans Barinovs <zans.barinovs@gmail.com>
2021-12-11 17:16:06 +0200

    added bot

--- bot/bot.py
+++ bot/bot.py
@@ -1,1 +1,32 @@
-print("hello world")
+import telebot
+import bot_settings
+
+bot = telebot.TeleBot(bot_settings.token)
+
+@bot.message_handler(commands=['main'])
+def main(message):
+    bot.send_message(message.chat.id, 'It works!')
+
+
+bot.infinity_polling()
+# import telebot
+# import bot_settings
+# from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+# bot = telebot.TeleBot(config.TOKEN)
+
+# def gen_markup():#generates Inline keyboard ansewrs
+#     markup = InlineKeyboardMarkup()
+#     markup.row_width = 2
+#     markup.add(InlineKeyboardButton("Robots", callback_data="cb_Robots"),
+#                                InlineKeyboardButton("Sculptures", callback_data="cb_Sculptures"))
+#     return markup
+
+# @bot.message_handler(commands=['start'])
+# def send_welcome(message):
+#     bot.reply_to(message, """
+#     Hello I am your RIBOCA assistant
+#     Pick the topic which you interested in""", reply_markup=gen_markup())
+# #welcome message and Inline keyboard init
+
+# bot.infinity_polling()#bot init
---
+++ bot/bot_settings.py
@@ -0,0 +1,1 @@
+token = '5070241180:AAG7G4EDldoX5TyCvG8UfNJ_HO8ewQFOjbQ'#Bot token from @FatherBot
