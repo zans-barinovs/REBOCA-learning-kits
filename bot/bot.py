1fd78367dc4de0fcb9cf8da031cad1fc0683ad13
Zans Barinovs <zans.barinovs@gmail.com>
2021-12-12 00:05:35 +0200

    made most popular kits to appear

--- bot/bot.py
+++ bot/bot.py
@@ -8,46 +8,56 @@ bot = telebot.TeleBot(bot_settings.token)
 def start(message):
     getting_kit_way = telebot.types.InlineKeyboardMarkup(row_width=1)
     getting_kit_way.add(
-        telebot.types.InlineKeyboardButton("Show most popular learning kits", callback_data="most_popular_kit"),
-        telebot.types.InlineKeyboardButton("Serch learning kit", callback_data="search_kit")
+        telebot.types.InlineKeyboardButton(
+            "Show most popular learning kits", callback_data="most_popular_kit"),
+        telebot.types.InlineKeyboardButton(
+            "Serch learning kit", callback_data="search_kit")
     )
-    
+
     bot.send_message(message.chat.id, 'Hello! \nI\'m RIBOCA assistant bot.')
-    bot.send_message(message.chat.id, 'Here is what I can do.', reply_markup=getting_kit_way)   
+    bot.send_message(message.chat.id, 'Here is what I can do.',
+                     reply_markup=getting_kit_way)
+
+
+def get_line_count(file): #SOMEHOW DOESNT FUCKING WORK
+    line_count = 0
+    for line in file:
+        if line != "\n":
+            line_count += 1
+
+    return line_count
+
+
+def most_popular_kit(data):
+    file = open("..\data\learning_kits\most_popular_learning_kits.txt")
     
-def FunctionName(args):
-    
+    file_lines = file.readlines()
+
+    most_popular_kits = telebot.types.InlineKeyboardMarkup()
+    print(get_line_count(file))
+    for i in range(2): #NEED TO CHAINGE
+        one_kit = str(file_lines[i])
+        print(one_kit)
+        tmp = telebot.types.InlineKeyboardButton(
+            one_kit, callback_data=str("kitname" + one_kit))
+        print("1")
+        most_popular_kits.add(tmp)
+        print("1")
+
+    print("2")
+    bot.send_message(data.message.chat.id, 'Choose one for you',
+                     reply_markup=most_popular_kits)
+    print("3")
+
+    file.close()
+
 
 @bot.callback_query_handler(func=lambda data: True)
 def buttons_callbacks(data):
     if data.data == 'most_popular_kit':
-        bot.send_message(data.message.chat.id, 'Choose one for you')
+        most_popular_kit(data)
     elif data.data == "search_kit":
         bot.send_message(data.message.chat.id, 'Enter the topic please')
-        
-        
-    
-    
+
 
 bot.infinity_polling()
-# import telebot
-# import bot_settings
-# from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-# bot = telebot.TeleBot(config.TOKEN)
-
-# def gen_markup():#generates Inline keyboard ansewrs
-#     markup = InlineKeyboardMarkup()
-#     markup.row_width = 2
-#     markup.add(InlineKeyboardButton("Robots", callback_data="cb_Robots"),
-#                                InlineKeyboardButton("Sculptures", callback_data="cb_Sculptures"))
-#     return markup
-
-# @bot.message_handler(commands=['start'])
-# def send_welcome(message):
-#     bot.reply_to(message, """
-#     Hello I am your RIBOCA assistant
-#     Pick the topic which you interested in""", reply_markup=gen_markup())
-# #welcome message and Inline keyboard init
-
-# bot.infinity_polling()#bot init
